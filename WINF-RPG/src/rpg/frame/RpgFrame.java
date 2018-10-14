46b9d921e3280809a2793b3682cba32cdccdfdeb
Peter <trompeten.peter@gmx.de>
2018-10-14 17:36:20 +0200

    commit

--- WINF-RPG/src/rpg/frame/RpgFrame.java
+++ WINF-RPG/src/rpg/frame/RpgFrame.java
@@ -6,8 +6,9 @@ import javax.swing.JPanel;
 import rpg.menue.panel.MenuePanel;
 
 public class RpgFrame extends JFrame {
-
-	private JPanel contentPane;
+	
+	public MenuePanel mp;
+	
 
 	/**
 	 * Launch the application.
@@ -16,7 +17,7 @@ public class RpgFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public RpgFrame() {
-		MenuePanel mp = new MenuePanel();
+		mp = new MenuePanel();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 600);
 		setContentPane(mp);
--- WINF-RPG/src/rpg/game/panel/GamePanel.java
+++ WINF-RPG/src/rpg/game/panel/GamePanel.java
@@ -4,9 +4,7 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
+
 	public GamePanel() {
 		setLayout(null);
 		
