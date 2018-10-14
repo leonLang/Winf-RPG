09adfe39935705a6b11d1cd8297b70318534569b
leonl <leonlanglang@gmail.com>
2018-10-14 14:35:26 +0200

    dfddd

--- WINF-RPG/src/rpg/frame/RpgFrame.java
+++ WINF-RPG/src/rpg/frame/RpgFrame.java
@@ -1,11 +1,7 @@
 package rpg.frame;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import rpg.menue.panel.MenuePanel;
 
