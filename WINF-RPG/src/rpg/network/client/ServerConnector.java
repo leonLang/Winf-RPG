d41e9908922542bafc927857bb5b5ea74d6c96fa
Peter <trompeten.peter@gmx.de>
2018-10-14 14:49:55 +0200

    connector

--- WINF-RPG/src/rpg/network/client/ServerConnector.java
+++ WINF-RPG/src/rpg/network/client/ServerConnector.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class ServerConnector {
 	public ServerConnector() throws IOException {
@@ -20,29 +20,33 @@ public class ServerConnector {
 		PrintWriter os = null;
 
 		br = new BufferedReader(new InputStreamReader(System.in));
-		s1 = new Socket(address, port);
-		is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
-		os = new PrintWriter(s1.getOutputStream());
-
-		String response = "";
 		try {
-			line = br.readLine();
-			while (line.compareTo("QUIT") != 0) {
-				os.println(line);
-				os.flush();
-				response = is.readLine();
-				System.out.println("Server Response : " + response);
-				line = br.readLine();
+			s1 = new Socket(address, port);
+			is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+			os = new PrintWriter(s1.getOutputStream());
 
+			String response = "";
+			try {
+				line = br.readLine();
+				while (line.compareTo("QUIT") != 0) {
+					os.println(line);
+					os.flush();
+					response = is.readLine();
+					System.out.println("Server Response : " + response);
+					line = br.readLine();
+
+				}
+			} catch (IOException e) {
+				System.out.println("Socket read Error");
+			} finally {
+				is.close();
+				os.close();
+				br.close();
+				s1.close();
+				System.out.println("Connection Closed");
 			}
-		} catch (IOException e) {
-			System.out.println("Socket read Error");
-		} finally {
-			is.close();
-			os.close();
-			br.close();
-			s1.close();
-			System.out.println("Connection Closed");
+		} catch (ConnectException e) {
+			System.err.println("Unable to connect: Connection refused: connect");
 		}
 	}
 }
